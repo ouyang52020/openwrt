67d16c540d05dca63f02bcdfc1c847a93ebe801b
SirPdboy <63764630+sirpdboy@users.noreply.github.com>
2020-08-04 20:56:32 +0800

    update trojan

--- scripts/update_package.sh
+++ scripts/update_package.sh
@@ -10,6 +10,7 @@ sed -i 's/"luci.fs"/"luci.sys".net/g' package/new/luci-app-beardropper/luasrc/mo
 sed -i '/firewall/d' package/new/luci-app-beardropper/root/etc/uci-defaults/luci-beardropper
 mv package/new/luci-app-beardropper/po/zh_Hans   package/new/luci-app-beardropper/po/zh-cn
 rm -rf ./package/lean/luci-theme-argon
+rm -rf ./package/lean/trojan
 rm -rf ./package/lean/luci-theme-opentomcat
 svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/new/luci-app-ssr-plus
 git clone -b master --single-branch https://github.com/tty228/luci-app-serverchan package/new/luci-app-serverchan
@@ -24,6 +25,7 @@ sed -i 's/), 5)/), 48)/g' package/new/luci-app-clash/luasrc/controller/clash.lua
 svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-app-passwall package/new/luci-app-passwall
 svn co https://github.com/Lienol/openwrt-package/trunk/package/trojan-go package/new/trojan-go
 svn co https://github.com/Lienol/openwrt-package/trunk/package/trojan-plus package/new/trojan-plus
+svn co https://github.com/Lienol/openwrt-package/trunk/package/trojan package/new/trojan
 svn co https://github.com/jerrykuku/luci-app-vssr/trunk/  package/new/luci-app-vssr
 svn co https://github.com/jerrykuku/luci-app-jd-dailybonus/trunk/ package/new/luci-app-jd-dailybonus
 chmod -R 755 ./
