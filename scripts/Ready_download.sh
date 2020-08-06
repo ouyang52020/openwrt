c2e7bebc1d38c45d0c11d03e42b7ca9d449d629f
SirPdboy <63764630+sirpdboy@users.noreply.github.com>
2020-08-07 02:34:05 +0800

    set

--- scripts/Ready_download.sh
+++ scripts/Ready_download.sh
@@ -29,7 +29,7 @@ sed -i 's/家庭云//g' package/lean/luci-app-familycloud/luasrc/controller/fami
 #sed -i '/filter_aaaa/d' package/network/services/dnsmasq/files/dhcp.conf
 sed -i 's/$(VERSION_DIST_SANITIZED)/$(shell date +%Y%m%d)-$(VERSION_DIST_SANITIZED)/g' include/image.mk
 wget -P package/lean/autocore/files  https://raw.githubusercontent.com/siropboy/other/master/patch/autocore/files/index.htm
-wget -P package/lean/autocore/files https://raw.githubusercontent.com/siropboy/other/master/patch/autocore/files/sbin/cpuinfo
+#wget -P package/lean/autocore/files https://raw.githubusercontent.com/siropboy/other/master/patch/autocore/files/sbin/cpuinfo
 sed -i '/DISTRIB_REVISION/d' ./package/base-files/files/etc/openwrt_release
 echo "DISTRIB_REVISION='S$(date +%Y.%m.%d) Sirpdboy'" >> ./package/base-files/files/etc/openwrt_release
 ./scripts/feeds install -a
